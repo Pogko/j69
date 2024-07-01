71ed2b94f2fd6d1bcda8c666059825f4c3f94c71
Pogko Nwc <rahmatnxn062@gmail.com>
2024-07-01 02:01:50 +0000

    j69

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,15 @@ import { getFirestore } from "firebase/firestore"
 import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDB5E5_nfKe0zzpHbPvM2Xe5xl-RUNXA_0",
-  authDomain: "j69-52ddf.firebaseapp.com",
-  projectId: "j69-52ddf",
-  storageBucket: "j69-52ddf.appspot.com",
-  messagingSenderId: "982445970054",
-  appId: "1:982445970054:web:9dc1713a0e43d3a963f707",
-  measurementId: "G-GQKWQQMDCS"
-};
+    apiKey: "AIzaSyDB5E5_nfKe0zzpHbPvM2Xe5xl-RUNXA_0",
+    authDomain: "j69-52ddf.firebaseapp.com",
+    projectId: "j69-52ddf",
+    storageBucket: "j69-52ddf.appspot.com",
+    messagingSenderId: "982445970054",
+    appId: "1:982445970054:web:5a8d560588a8110863f707",
+    measurementId: "G-Z5BMQX8MHC"
+  };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
